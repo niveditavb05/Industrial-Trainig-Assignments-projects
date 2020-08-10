8cd451cefd58fd882c7c56186545bd77a5aec187
niveditavb05 <68593325+niveditavb05@users.noreply.github.com>
2020-08-10 07:59:37 +0530

    Delete package-search-index.js

--- inventorymangagementsystem/dist/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.main.controller"},{"l":"com.main.DAO"},{"l":"com.main.dto"},{"l":"com.main.view"},{"l":"inventorymangagementsystem"}]
